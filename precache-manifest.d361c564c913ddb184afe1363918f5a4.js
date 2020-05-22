ee925c5bc844539fd9516b3223242ed0df8f2585
wonkyun bang <wkbang@gmail.com>
2020-05-21 20:23:09 -0400

    Initial commit2

---
+++ precache-manifest.d361c564c913ddb184afe1363918f5a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d51b11c423eed4baebe416925227a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b35624556b0084c7cd",
+    "url": "/static/css/main.aba9b278.chunk.css"
+  },
+  {
+    "revision": "e39663272ce062bac417",
+    "url": "/static/js/2.bd6ac36f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.bd6ac36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b35624556b0084c7cd",
+    "url": "/static/js/main.0f62d80e.chunk.js"
+  },
+  {
+    "revision": "b42741beaf87622b82ac",
+    "url": "/static/js/runtime-main.3c2ecbb5.js"
+  },
+  {
+    "revision": "1dd71612fd743c00f125158206df8518",
+    "url": "/static/media/codexRxLogo.1dd71612.jpeg"
+  },
+  {
+    "revision": "e5c4468d5675419e803026a67bb05741",
+    "url": "/static/media/visualization.e5c4468d.png"
+  }
+]);
